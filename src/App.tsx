4c14959a994f1c574f47a40ddd4d5b1990c5ea28
Daniel Prada <danielprada2006@gmail.com>
2025-07-03 17:56:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white text-4xl p-8 min-h-screen flex items-center justify-center">
+      If you see red, Tailwind is working!
+    </div>
+  );
+}
+
+export default App;
